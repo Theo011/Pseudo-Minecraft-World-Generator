84571114f04b7aa1595c2cbd226c4c24ea5b2d79
Thodoris Kalogeropoulos <teodragon97@hotmail.gr>
2022-06-04 12:46:25 +0300

    engine

--- src/engine/camera.h
+++ src/engine/camera.h
@@ -1,27 +1,27 @@
-#pragma once
-
-#include <vector>
+#ifndef CAMERA_H
+#define CAMERA_H
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
 enum Camera_Movement {
     FORWARD,
     BACKWARD,
     LEFT,
-    RIGHT,
-	UP,
-	DOWN
+    RIGHT
 };
 
 // Default camera values
-const float YAW = -90.0f;
-const float PITCH = 0.0f;
-const float SPEED = 12.5f;
-const float SENSITIVITY = 0.2f;
-const float ZOOM = 70.0f;
+const float YAW         = -90.0f;
+const float PITCH       =  0.0f;
+const float SPEED       =  20.5f;
+const float SENSITIVITY =  0.1f;
+const float ZOOM        =  45.0f;
+
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
 class Camera
@@ -50,7 +50,6 @@ public:
         Pitch = pitch;
         updateCameraVectors();
     }
-	
     // constructor with scalar values
     Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
     {
@@ -79,10 +78,6 @@ public:
             Position -= Right * velocity;
         if (direction == RIGHT)
             Position += Right * velocity;
-		if (direction == UP)
-			Position += Up * velocity;
-		if (direction == DOWN)
-			Position -= Up * velocity;
     }
 
     // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
@@ -91,7 +86,7 @@ public:
         xoffset *= MouseSensitivity;
         yoffset *= MouseSensitivity;
 
-        Yaw += xoffset;
+        Yaw   += xoffset;
         Pitch += yoffset;
 
         // make sure that when pitch is out of bounds, screen doesn't get flipped
@@ -114,7 +109,7 @@ public:
         if (Zoom < 1.0f)
             Zoom = 1.0f;
         if (Zoom > 45.0f)
-            Zoom = 45.0f;
+            Zoom = 45.0f; 
     }
 
 private:
@@ -129,6 +124,7 @@ private:
         Front = glm::normalize(front);
         // also re-calculate the Right and Up vector
         Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-        Up = glm::normalize(glm::cross(Right, Front));
+        Up    = glm::normalize(glm::cross(Right, Front));
     }
 };
+#endif
